0b1834e105ceace5aa0bc9a6755cf03f8e8a1cae
Maria Eltsova <me7886@student.uni-lj.si>
2023-08-25 09:39:20 +0200

    config for router deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+  <Router basename={import.meta.env.DEV ? '/' : '/productivity-app/'}>
     <App />
   </Router>
 )
